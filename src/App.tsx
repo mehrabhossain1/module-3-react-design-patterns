872972606deb40d299561fb29a802396dad1f634
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-01-12 21:14:48 +0600

    3.5

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
+import { useContext } from 'react'
 import './App.css'
-import Calculator from './components/Calculator'
+import { TThemeContext, ThemeContext } from './context/ThemeProvider'
+import { MenuItem, MenuList } from './components/Menu'
 
 function App() {
+  const { dark, setDark } = useContext(ThemeContext) as TThemeContext
+  console.log(dark)
+
   return (
-    <>
-      <Calculator />
-    </>
+    <div
+      className={`h-screen w-full flex justify-center items-center ${
+        dark ? 'bg-black' : 'bg-white'
+      }`}
+    >
+      <button onClick={() => setDark(!dark)}>Toggle</button>
+      {/* <UseRef /> */}
+
+      <MenuList>
+        <MenuItem></MenuItem>
+      </MenuList>
+    </div>
   )
 }
 
